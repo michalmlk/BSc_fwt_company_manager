5c7928aec2edbcaa6611f386b0c167dc3ac91ea7
michalmlk <michal.melka1@gmail.com>
2023-09-15 23:22:42 +0200

    change assigntruck modal content to datatable

--- client/src/components/organisms/Modal/AssignTruckModalContent.tsx
+++ client/src/components/organisms/Modal/AssignTruckModalContent.tsx
@@ -6,6 +6,8 @@ import { Dropdown } from 'primereact/dropdown';
 import { Employee } from '../../../common/model';
 import { ModalFooter } from './Modal/Modal';
 import { toast } from 'react-toastify';
+import { DataTable, DataTableSelectionChangeEvent } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 const AssignTruckModalContent: React.FC<{ employee: Employee | undefined; onClose: () => void }> = ({
     employee,
@@ -13,7 +15,7 @@ const AssignTruckModalContent: React.FC<{ employee: Employee | undefined; onClos
 }) => {
     const service = new ManagerService();
     const [trucks, setTrucks] = useState<Truck[]>([]);
-    const [selectedTruck, setSelectedTruck] = useState<Truck | undefined>();
+    const [selectedTruck, setSelectedTruck] = useState<Truck | undefined>(undefined);
     const queryClient = useQueryClient();
 
     const { data } = useQuery({
@@ -26,7 +28,7 @@ const AssignTruckModalContent: React.FC<{ employee: Employee | undefined; onClos
     useEffect(() => {
         if (data && employee) {
             console.log(data);
-            setTrucks(data.filter((d) => d.driverId == null));
+            setTrucks(data);
             setSelectedTruck(data.filter((t) => t.id == employee.truckId)[0]);
         }
     }, [data, employee]);
@@ -48,14 +50,18 @@ const AssignTruckModalContent: React.FC<{ employee: Employee | undefined; onClos
 
     return (
         <>
-            <Dropdown
-                value={selectedTruck}
-                onChange={(e) => setSelectedTruck(trucks.find((t) => t.id === e.value.id))}
-                options={trucks}
-                optionLabel="model"
-                placeholder="Select Truck"
-                className="w-12 mb-4"
-            />
+            <label htmlFor="employeeTruck">Available trucks</label>
+            <DataTable
+                value={trucks!}
+                selectionMode="single"
+                selection={selectedTruck}
+                onSelectionChange={(e: DataTableSelectionChangeEvent<Truck>) => setSelectedTruck(e.value)}
+            >
+                <Column field="id" header="Code" />
+                <Column field="model" header="Name" />
+                <Column field="registrationNumber" header="Category" />
+                <Column field="techState" header="Quantity" />
+            </DataTable>
             <ModalFooter
                 onClose={onClose}
                 onConfirm={handleAssignTruckConfirm}
