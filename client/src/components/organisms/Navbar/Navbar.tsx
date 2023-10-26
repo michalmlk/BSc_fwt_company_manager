2fe08168338df0b61908a86612bba84f343b2372
michalmlk <michal.melka1@gmail.com>
2023-10-26 19:40:57 +0200

    hide reports tab

--- client/src/components/organisms/Navbar/Navbar.tsx
+++ client/src/components/organisms/Navbar/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC = () => {
         { label: 'Transports', icon: 'pi pi-fw pi-calendar', command: () => navigateToPath('/deliveries') },
         { label: 'Drivers', icon: 'pi pi-fw pi-id-card', command: () => navigateToPath('/employees') },
         { label: 'Machine Park', icon: 'pi pi-fw pi-truck', command: () => navigateToPath('/trucks') },
-        { label: 'Reports', icon: 'pi pi-fw pi-file', command: () => navigateToPath('/Reports') },
+        // { label: 'Reports', icon: 'pi pi-fw pi-file', command: () => navigateToPath('/Reports') },
     ];
 
     const navigateToPath = (path: string) => {
